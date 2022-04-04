4c4274f71a4d676dc88d7e141175d65b8b79821e
MUGIRANEZA JOHN <52497006+mujohn26@users.noreply.github.com>
2022-04-04 22:25:41 +0200

    fetch data

--- src/controllers/etherscan.controller.js
+++ src/controllers/etherscan.controller.js
@@ -4,10 +4,23 @@ import EtherscanService from '../services/etherscan.service';
 class EtherscanWalletController{
 
   static async getWalletsData(req, res) {
-    const address = "0x6ec5ec42e79f57d42e80b6d8ba113903accda539"
-    const data = await axios.get(`https://api.etherscan.io/api?module=account&action=balancemulti&address=${address}&tag=latest&apikey=H2CTXGJCY5KBHFUFY2E873MI7H2WYKKJWB`)
-    const result = data.data.result;
+    const addresses = [
+      "0x6ec5ec42e79f57d42e80b6d8ba113903accda539",
+      "0x76f4fe172a1215f5e6cad9f614c4633a5c430d09",
+      "0x73e8d47767dd909ebc10f34ed186598b4ae4a71b",
+      "0xcf6bfadd4bef5bef62ca9cd114cc72fe11d74d8d",
+      "0xA8e43C4E98f2E39513c0286Ef45C4bF711778145",
+      "0x36879D9a59B18711e5C4e970dC8b98F4894f7667",
+      "0xFa4755640F537babaBE67e2220CD862b2D231591",
+      "0x8220F9a1267C1C4378b2c3e0307B9B712398BEcF",
+      "0xC2b1B510021fB1a5679F6F478b300b6ECaBC255c",
+      "0xc7845Be7d80d0c213357350898E0870771C7C649",
+      "0x51a72fC34781C6AFCC6da30Ad1204f618822C390"
+    ]
 
+    for (let index = 0; index < addresses.length; index++){
+    const data = await axios.get(`https://api.etherscan.io/api?module=account&action=balancemulti&address=${addresses[index]}&tag=latest&apikey=H2CTXGJCY5KBHFUFY2E873MI7H2WYKKJWB`)
+      const result = data.data.result;
   if (result != null) {
   let count = 0;
   result.forEach(async(e) => {
@@ -16,7 +29,6 @@ class EtherscanWalletController{
     balance: e.balance
    }
     const walletData = await EtherscanService.findOneWallet(e.account)
-    console.log('=-=-=-=-==', walletData)
 
     if (walletData.account == e.account) {
       const updateResponse = await EtherscanService.updateWallet(data, walletData.account)
@@ -64,9 +76,9 @@ class EtherscanWalletController{
     }
   });
     
-    res.status(200).json({ message: "Wallets were added successfully" })
-    
-    }
+  
+}}
+res.status(200).json({ message: "Wallets were added successfully" })
   }
 }
 
--- src/controllers/wallets.controller.js
+++ src/controllers/wallets.controller.js
@@ -7,36 +7,42 @@ class WalletsController{
   static async fetchWalletsData(req, res) {
     const walletData = await WalletsService.fetchWallets()
     let data = []
-    walletData.forEach(async (wallet) => {
-      const walletBlocks = await WalletsService.fetchWalletsBlocks(wallet.account)
+    for (const wallet of walletData) {
+      const walletBlocks = await WalletsService.fetchWalletsBlocks(wallet.dataValues.account)
       let blocks = []
-      await walletBlocks.forEach((block) => {
+      for (const block of walletBlocks) {
+        const singleBlock = block.dataValues
         const blockData = {
-          blockNumber: block.blockNumber,
-          timestamp: block.timestamp,
-          blockHash: block.blockHash,
-          walletId: block.WalletId,
-          from: block.from,
-          to: block.to,
-          value: block.value,
-          gas: block.gas,
-          gasPrice: block.gasPrice,
-          gasUsed: block.gasUsed,
-          hash: block.hash
+          blockNumber: singleBlock.blockNumber,
+          timestamp: singleBlock.timestamp,
+          blockHash: singleBlock.blockHash,
+          walletId: singleBlock.WalletId,
+          from: singleBlock.from,
+          to: singleBlock.to,
+          value: singleBlock.value,
+          gas: singleBlock.gas,
+          gasPrice: singleBlock.gasPrice,
+          gasUsed: singleBlock.gasUsed,
+          hash: singleBlock.hash
         }
+
         blocks.push(blockData)
-      })
+      }
       const walletAndBlocks = {
         wallet:{
-          account: wallet.account,
-          balance: wallet.balance
+          account: wallet.dataValues.account,
+          balance: wallet.dataValues.balance
         },
         blocks: blocks
       }
       data.push(walletAndBlocks)
-
-    });
-    console.log(data)
+    
+    }
+    res.status(200).json(
+      {
+        message: "Wallets were fetched successfully",
+        data: data
+      })
 }
 }
 
